1f517ebac53bc075aa8e91862ce40780bec1168b
gowdaamith <amithgowda1772@gmail.com>
2026-01-23 23:07:52 +0530

    Add EC2 instance variables for Terraform
    Fix variable definitions and typos in variable.tf

---
+++ AWS_ec2/Phase1/terraform-phase1/variable.tf
@@ -0,0 +1,12 @@
+variable "instance_name" {
+  default = "ec2-phase1-terraform"
+}
+variable "instance_type"{
+  deafult = "t2.miro"
+}
+variable "amiid"{
+  deafult={}
+}
+varaible "key_name"{
+  default = "ec2-phase1-key"
+}
